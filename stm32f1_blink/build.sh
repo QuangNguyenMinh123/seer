888892f515bd99766f8a872313518b6282e47abc
QuangNguyenMinh123 <100016403+QuangNguyenMinh123@users.noreply.github.com>
2025-09-12 09:56:16 +0700

    u

---
+++ stm32f1_blink/build.sh
@@ -0,0 +1,11 @@
+TOP_DIR=${PWD}
+cd ..
+git submodule update --init --recursive
+cd stm32f1_blink
+# install package
+sudo apt install gcc-arm-none-eabi
+# make
+make
+# make flash, assume that openocd is installed by running install_openocd.sh
+make flash JLINK_CFG=${PWD}/../openocd/tcl/interface/jlink.cfg
+
